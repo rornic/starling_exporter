b86846569c20aea5d62651963e9ce765e007adad
Rory N <rorynickolls@gmail.com>
2024-05-03 19:09:55 +0100

    refactor: separation of concerns in main

--- cmd/starling_exporter.go
+++ cmd/starling_exporter.go
@@ -11,16 +11,13 @@ import (
 	"github.com/rornic/starlingexporter/internal/metrics"
 )
 
-func main() {
-	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
-	slog.SetDefault(logger)
-
+func initialiseStarlingClient() client.StarlingClient {
 	accessToken := os.Getenv("STARLING_ACCESS_TOKEN")
 	if accessToken == "" {
 		slog.Error("STARLING_ACCESS_TOKEN is not set. Exiting.")
 		os.Exit(1)
 	}
-	slog.Info("using access token from environment")
+	slog.Info("found access token in environment")
 
 	endpoint := "https://api.starlingbank.com/api/v2"
 	sandbox := strings.ToLower(os.Getenv("STARLING_SANDBOX")) == "true"
@@ -30,12 +27,19 @@ func main() {
 	}
 
 	client := client.NewStarlingHttpClient(accessToken, endpoint)
-	metrics.Record(&client)
+	return &client
 
+}
+
+func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+
+	client := initialiseStarlingClient()
+	metrics.Record(client)
 	http.Handle("/metrics", promhttp.Handler())
 
 	slog.Info("listening on :8080")
-
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		slog.Error("error running server: %v\n", err)
